4c3039f46d502a6775bf92b1399cb8223ddba7d3
nerd8932 <samir.amin8932@gmail.com>
2025-03-16 17:08:01 -0300

    Fixed error in pathing for build scripts.

--- backend/build.ps1
+++ backend/build.ps1
@@ -1,11 +1,10 @@
 Write-Host 'Installing...'
 
 pyinstaller --paths="..\.venv\Lib\site-packages\" --icon="..\gui\icons\favicon.ico" app.py -n "SonoGrapher Backend v1.0" -D --optimize 2 --noconfirm
+Move-Item -Path "./dist/SonoGrapher Backend v1.0/" -Destination "../" -Force
 
 New-Item -ItemType Directory -Force -Path "../SonoGrapher Backend v1.0/backend/logs/"
 New-Item -ItemType Directory -Force -Path "../SonoGrapher Backend v1.0/backend/uploaded/"
-
-Move-Item -Path "./dist/SonoGrapher Backend v1.0/" -Destination "../SonoGrapher Backend v1.0/" -Force
 
 Copy-Item -Path "./app.py" -Destination "../SonoGrapher Backend v1.0/" -Force
 Copy-Item -Path "./database.py" -Destination "../SonoGrapher Backend v1.0/" -Force
--- backend/build.sh
+++ backend/build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 echo 'Installing...'
 pyinstaller --paths=..\.venv\Lib\site-packages\ --icon=..\gui\icons\favicon.ico app.py -n "SonoGrapher Backend v1.0" -D --optimize 2 --noconfirm
+mv "./dist/SonoGrapher Backend v1.0/" "../"
 mkdir "../SonoGrapher Backend v1.0/backend/logs/"
 mkdir "../SonoGrapher Backend v1.0/backend/uploaded/"
-mv "./dist/SonoGrapher Backend v1.0/" "../SonoGrapher Backend v1.0/"
 cp "./app.py" "../SonoGrapher Backend v1.0/"
 cp "./database.py" "../SonoGrapher Backend v1.0/"
 cp "./SonoGrapherTemplate.docx" "../SonoGrapher Backend v1.0/backend/"
