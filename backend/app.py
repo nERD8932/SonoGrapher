ea00dcddc1ce76d7ba3f13e2f05b2016396cca2a
nerd8932 <samir.amin8932@gmail.com>
2025-03-16 04:01:42 -0300

    Added CLI arguments for backend server, cleaned up imports, fixed a few bugs and added system_prompt.txt for customization.

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,9 @@
-import uuid
-
-import flask
 import torch
-import torchaudio
-import io
 from flask import Flask, request, Response, abort, send_file
 import ollama
 import whisper
 from openai import OpenAI
-import soundfile as sf
-import pydub
-import scipy
-import numpy as np
+from pprint import pprint
 from dotenv import load_dotenv
 import logging
 from logging.handlers import RotatingFileHandler
@@ -24,83 +16,50 @@ from docxtpl import DocxTemplate
 
 
 class Backend:
-    def __init__(self):
-        # Load environment variables
-        load_dotenv()
+    def __init__(self,
+                 use_local_stt=False,
+                 use_local_llm=False,
+                 local_llm="deepseek-r1:7b",
+                 local_stt="base",
+                 openai_llm="gpt-4o-mini",
+                 openai_stt="whisper-1",
+                 openai_key=os.environ.get("OPENAI_API_KEY", "")):
+
         # Set up logging obj
         self.log = self.logging_bp()
         # Local or API flag
-        self.use_local = True
+        self.use_local_stt = use_local_stt
+        self.use_local_llm = use_local_llm
+        self.openai_llm = openai_llm
+        self.openai_stt = openai_stt
+        self.local_llm = local_llm
+        self.local_stt = local_stt
+        self.openai_key = openai_key
+
+        if not self.use_local_llm or not self.use_local_stt:
+            if self.openai_key == "":
+                self.log.critical("You forgot to set your OpenAI API key.")
+                exit(-9)
 
         # Load API Keys from db
         self.db = create_connection('./backend/api_keys.sqlite', self.log)
         self.api_keys = {x[0]: x[1] for x in self.db.execute('select * from api_keys').fetchall()}
 
         # Load Speech-To-Text through Whisper
-        self.stt = self.load_whisper(local=self.use_local)
+        self.stt = self.load_whisper()
 
         # Load Deepseek-R1 into memory.
-        self.system_prompt = '''
-You need to extract sonography information from a given text and output it in the following JSON format.
-If the information does not exist, fill the section with "UNKNOWN" or "NOT APPLICABLE". Here is the format to follow:
-{
-  "patientInfo": {
-    "name": "<str: Patient Name>",
-    "sex": "<str: Gender>",
-    "age": "<int: Age>",
-    "date": "<str: Current Examination Date>",
-    "referringPhysician": "<str: Referring Physician>",
-    "uniqueId": "<str: Unique ID>"
-  },
-  "examinationDetails": {
-    "type": "<str: Examination Type>",
-    "lastMenstrualPeriod": "<str: LMP Date>",
-    "estimatedDeliveryDate": "<str: EDD>",
-    "gestationalAge": {
-      "weeks": "<int: Weeks>",
-      "days": "<int: Days>"
-    }
-  },
-  "findings": "<str: Free-text description of observations (e.g., organ status, fetal measurements, anomalies, fluid presence, etc.)>",
-  "measurements": {
-    "fetalParameters": [
-      {
-        "name": "<str: Parameter Name (e.g., BPD, HC, FL)>",
-        "value": "<float: Value>",
-        "unit": "<str: Unit>"
-      }
-    ],
-    "organMetrics": [
-      {
-        "organ": "<str:Organ Name (e.g., Uterus, Kidney)>",
-        "size": "<str: Dimensions>",
-        "thickness": "<str: Thickness>",
-        "other": "<str: Additional metrics>"
-      }
-    ],
-    "dopplerIndices": [
-      {
-        "artery": "<str: Artery Name>",
-        "PI": "<str: Pulsatility Index>",
-        "RI": "<str: Resistive Index>",
-        "SD": "<str: Systolic/Diastolic Ratio>"
-      }
-    ]
-  },
-  "comments": list: [
-    "<str: Clinical summary or notable observations >"
-  ],
-  "clinicalCorrelation": list: [
-    "<str: Recommendations for follow-up or additional tests>"
-    ],
-}
+        self.system_prompt = ""
+        with open('./backend/system_prompt.txt', 'r') as f:
+            self.system_prompt = f.read()
 
-Do not deviate from the format.
-'''
-        ollama.generate(model='deepseek-r1:7b', keep_alive=20.0, system=self.system_prompt)
+        if self.use_local_llm:
+            ollama.generate(model=self.local_llm, keep_alive=20.0, system=self.system_prompt)
+
 
         self.app = Flask(__name__)
         self.setup_routes()
+        # pprint(vars(self))
 
     def setup_routes(self):
         @self.app.route('/api/generate-report', methods=["POST"])
@@ -127,8 +86,18 @@ Do not deviate from the format.
                     abort(400, "No file attached!")
 
                 self.log.info(f"Started LLM generation.", extra=extra)
-                result = ollama.generate(prompt=f"{self.system_prompt}\nHere is the paragraph:\n {data}", model='deepseek-r1:7b', system=self.system_prompt)
-                
+
+                prompt = f"{self.system_prompt}\nHere is the paragraph:\n {data}"
+                if self.use_local_llm:
+                    result = ollama.generate(prompt=prompt, model=self.local_llm, system=self.system_prompt)
+                else:
+                    client = OpenAI(api_key=self.openai_key)
+                    response = client.responses.create(
+                        model=self.openai_llm,
+                        instructions=self.system_prompt,
+                        input=prompt)
+                    result = {'response': response.output_text}
+
                 self.log.info(f"LLM finished generating response, creating document...", extra=extra)
                 json_obj = json.loads(result['response'].split('```json\n')[1].split('```')[0])
                 dt = DocxTemplate("./backend/SonoGrapherTemplate.docx")
@@ -149,12 +118,12 @@ Do not deviate from the format.
             with open(f'./backend/uploaded/{ak}.wav', mode='bx') as f:
                 f.write(file)
 
-            if self.use_local:
+            if self.use_local_stt:
                 result = self.stt.transcribe(f'./backend/uploaded/{ak}.wav')
             else:
                 audio_file = open(f'./backend/uploaded/{ak}.wav', mode='rb')
                 transcription = self.stt.audio.transcriptions.create(
-                    model="whisper-1",
+                    model=self.openai_stt,
                     file=audio_file,
                     response_format="text"
                 )
@@ -222,22 +191,88 @@ Do not deviate from the format.
         if key not in self.api_keys:
             abort(401, "Invalid API key.")
 
-    def load_whisper(self, local=False):
-        if local:
+    def load_whisper(self):
+        if self.use_local_stt:
             try:
                 if not os.path.exists('./backend/uploaded/'):
                     os.mkdir('./backend/uploaded/')
                 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
                 self.log.info(f'Using {"GPU" if device.type == "cuda" else "CPU"} device for Speech-to-Text.')
-                return whisper.load_model("base", device=device)
+                return whisper.load_model(self.local_stt, device=device)
             except Exception as e:
                 self.log.critical(e)
                 exit(-10)
         else:
-            client = OpenAI(api_key=os.environ.get('OPENAI_API_KEY'))
+            client = OpenAI(api_key=self.openai_key)
             return client
 
+if __name__ == "__main__":
+    # Load environment variables
+    load_dotenv()
 
+    parser = argparse.ArgumentParser(
+        prog='SonoGrapher Backend',
+        description='SonoGrapher Backend works by hosting a simple Flask-based server that '
+                    '(with the help of Ollama and OpenAI\'s Whisper) lets you convert text/audio '
+                    'sonography session data into a cohesive docx report in just a few clicks. It\'s highly '
+                    'customizable and has been built with ease-of-use and robustness in mind.',
+        epilog='Feel free to contact the developer (me) @nerd8932 on Discord or GitHub')
 
-backend = Backend()
-app = backend.app  # Expose Flask app instance
+    parser.add_argument('--use-local-llm',
+                        help='Use Ollama to run an LLM locally for prompting.',
+                        action='store_true',
+                        dest='use_local_llm')
+
+    parser.add_argument('--use-local-stt',
+                        help='Use Whisper to convert Speech-to-Text locally.',
+                        action='store_true',
+                        dest="use_local_stt")
+
+    local_stt_choices=['tiny', 'base', 'small', 'medium', 'large', 'turbo', 'tiny.en', 'base.en', 'small.en', 'medium.en']
+    parser.add_argument('--local-stt-model',
+                        type=str,
+                        default="base",
+                        help='Which Whisper model to use.',
+                        choices=local_stt_choices,
+                        dest="local_stt_model")
+
+    parser.add_argument('--local-llm-model',
+                        type=str,
+                        default="deepseek-r1:7b",
+                        help='Which Ollama model to use. Look at the possible options at https://ollama.com/search',
+                        dest="local_llm_model")
+
+    parser.add_argument('--openai-llm-model',
+                        type=str,
+                        default="gpt-4o-mini",
+                        help='Which OpenAI model to use for prompting. '
+                             'Look at the possible options at https://platform.openai.com/docs/models. '
+                             'Please be sure to use the --openai-api-key flag to set your API key, if you haven\'t set it '
+                             'already in your .env file or your environment variables.',
+                        dest="openai_llm_model")
+
+    parser.add_argument('--openai-stt-model',
+                        type=str,
+                        default="whisper-1",
+                        help='Which OpenAI model to use for Speech-to-Text. '
+                             'Look at the possible options at https://platform.openai.com/docs/models. '
+                             'Please be sure to use the --openai-api-key flag to set your API key, if you haven\'t set it '
+                             'already in your .env file or your environment variables.',
+                        dest="openai_stt_model")
+
+    parser.add_argument('--openai-api-key',
+                        type=str,
+                        default=os.environ.get("OPENAI_API_KEY", ""),
+                        help='API key for OpenAI models incase you\'re not running models locally.',
+                        dest="openai_api_key")
+
+    args = parser.parse_args()
+
+    backend = Backend(use_local_llm=args.use_local_llm,
+                      use_local_stt=args.use_local_stt,
+                      openai_llm=args.openai_llm_model,
+                      openai_stt=args.openai_stt_model,
+                      local_llm=args.local_llm_model,
+                      local_stt=args.local_stt_model,
+                      openai_key=args.openai_api_key)
+    backend.app.run()
